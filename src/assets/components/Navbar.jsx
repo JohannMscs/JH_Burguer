03027fc825cc7ffb3a8c59533f26652ac343f31c
JohannMscs <johann.moscoso@gmail.com>
2024-04-10 23:46:22 -0300

    mudando o local da Home

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-        <Link to="../../pages/Home">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="../../pages/Requests">Pedidos</Link>
     </nav>
   );
